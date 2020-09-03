59c11b498d139f49a1c952887191df34454740c1
shihaowei1 <1581258627@qq.com>
2020-09-03 10:27:09 +0800

    新建了数据库，控制器、url、视图的对应关系做了一下，但是没有连接显示数据库里的数据

--- MvcDemo/MvcDemo/Controllers/MovieDBsController.cs
+++ MvcDemo/MvcDemo/Controllers/MovieDBsController.cs
@@ -6,113 +6,129 @@ using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.Remoting.Messaging;
 using System.Web.Http;
+using System.Web.Http.Controllers;
 using System.Web.Http.Description;
+using System.Web.Mvc;
 using MvcDemo.Models;
 
 namespace MvcDemo.Controllers
 {
-    public class MovieDBsController : ApiController
+
+    public class MovieDBsController : Controller
     {
-        private MovieDBContext db = new MovieDBContext();
-
-        // GET: api/MovieDBs
-        public IQueryable<MovieDB> GetMovies()
+        public ActionResult Index() //右击Index，新建视图
         {
-            return db.Movies;
+            return View();
         }
 
-        // GET: api/MovieDBs/5
-        [ResponseType(typeof(MovieDB))]
-        public IHttpActionResult GetMovieDB(int id)
-        {
-            MovieDB movieDB = db.Movies.Find(id);
-            if (movieDB == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(movieDB);
-        }
-
-        // PUT: api/MovieDBs/5
-        [ResponseType(typeof(void))]
-        public IHttpActionResult PutMovieDB(int id, MovieDB movieDB)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != movieDB.ID)
-            {
-                return BadRequest();
-            }
-
-            db.Entry(movieDB).State = EntityState.Modified;
-
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!MovieDBExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return StatusCode(HttpStatusCode.NoContent);
-        }
-
-        // POST: api/MovieDBs
-        [ResponseType(typeof(MovieDB))]
-        public IHttpActionResult PostMovieDB(MovieDB movieDB)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            db.Movies.Add(movieDB);
-            db.SaveChanges();
-
-            return CreatedAtRoute("DefaultApi", new { id = movieDB.ID }, movieDB);
-        }
-
-        // DELETE: api/MovieDBs/5
-        [ResponseType(typeof(MovieDB))]
-        public IHttpActionResult DeleteMovieDB(int id)
-        {
-            MovieDB movieDB = db.Movies.Find(id);
-            if (movieDB == null)
-            {
-                return NotFound();
-            }
-
-            db.Movies.Remove(movieDB);
-            db.SaveChanges();
-
-            return Ok(movieDB);
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
-        private bool MovieDBExists(int id)
-        {
-            return db.Movies.Count(e => e.ID == id) > 0;
-        }
     }
+
+    //public class MovieDBsController : ApiController
+    //{
+
+    //    private MovieDBContext db = new MovieDBContext();
+
+    //    GET: api/MovieDBs
+    //    public IQueryable<MovieDB> GetMovies()
+    //    {
+    //        return db.Movies;
+    //    }
+
+    //    GET: api/MovieDBs/5
+    //    [ResponseType(typeof(MovieDB))]
+    //    public IHttpActionResult GetMovieDB(int id)
+    //    {
+    //        MovieDB movieDB = db.Movies.Find(id);
+    //        if (movieDB == null)
+    //        {
+    //            return NotFound();
+    //        }
+
+    //        return Ok(movieDB);
+    //    }
+
+    //    PUT: api/MovieDBs/5
+    //    [ResponseType(typeof(void))]
+    //    public IHttpActionResult PutMovieDB(int id, MovieDB movieDB)
+    //    {
+    //        if (!ModelState.IsValid)
+    //        {
+    //            return BadRequest(ModelState);
+    //        }
+
+    //        if (id != movieDB.ID)
+    //        {
+    //            return BadRequest();
+    //        }
+
+    //        db.Entry(movieDB).State = EntityState.Modified;
+
+    //        try
+    //        {
+    //            db.SaveChanges();
+    //        }
+    //        catch (DbUpdateConcurrencyException)
+    //        {
+    //            if (!MovieDBExists(id))
+    //            {
+    //                return NotFound();
+    //            }
+    //            else
+    //            {
+    //                throw;
+    //            }
+    //        }
+
+    //        return StatusCode(HttpStatusCode.NoContent);
+    //    }
+
+    //    POST: api/MovieDBs
+    //   [ResponseType(typeof(MovieDB))]
+    //    public IHttpActionResult PostMovieDB(MovieDB movieDB)
+    //    {
+    //        if (!ModelState.IsValid)
+    //        {
+    //            return BadRequest(ModelState);
+    //        }
+
+    //        db.Movies.Add(movieDB);
+    //        db.SaveChanges();
+
+    //        return CreatedAtRoute("DefaultApi", new { id = movieDB.ID }, movieDB);
+    //    }
+
+    //    DELETE: api/MovieDBs/5
+    //    [ResponseType(typeof(MovieDB))]
+    //    public IHttpActionResult DeleteMovieDB(int id)
+    //    {
+    //        MovieDB movieDB = db.Movies.Find(id);
+    //        if (movieDB == null)
+    //        {
+    //            return NotFound();
+    //        }
+
+    //        db.Movies.Remove(movieDB);
+    //        db.SaveChanges();
+
+    //        return Ok(movieDB);
+    //    }
+
+    //    protected override void Dispose(bool disposing)
+    //    {
+    //        if (disposing)
+    //        {
+    //            db.Dispose();
+    //        }
+    //        base.Dispose(disposing);
+    //    }
+
+    //    private bool MovieDBExists(int id)
+    //    {
+    //        return db.Movies.Count(e => e.ID == id) > 0;
+    //    }
+
+    //}
+
 }
