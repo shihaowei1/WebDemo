84bbf17f1479e832144520e149d793594bff5122
shihaowei1 <1581258627@qq.com>
2020-10-10 16:26:23 +0800

    更改提交

--- MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -9,9 +9,14 @@ namespace MvcMovie.Controllers
     public class HelloWorldController : Controller
     {
         // GET: HelloWorld
-        public string Index()
+        //public string Index()
+        //{
+        //    return "This is my <b>default</b> action...";
+        //}
+
+        public ActionResult Index()
         {
-            return "This is my <b>default</b> action...";
+            return View();
         }
 
         //GET: HelloWorld/Welcome
